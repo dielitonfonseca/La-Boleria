b209e413c8d0ae5b5b4590606cf4744ff78a9d5d
dielitonfonseca <dielitonskt@gmail.com>
2022-06-24 17:03:13 -0300

    feat: clients repository

--- src/controllers/cakesController.js
+++ src/controllers/cakesController.js
@@ -1,10 +1,17 @@
 import { cakesRepository } from "../repositories/cakesRepository.js";
 async function createCake(req, res) {
-	if (res.locals.name.lenght < 2) {
+	try {
+		const existingName = await cakesRepository.searchByName(res.locals.name);
+		if (existingName.rowCount >= 1) {
+			res.status(409).send("name already in database");
+			return;
+		}
+		await cakesRepository.insert(res.locals);
+		res.status(201).send(res.locals);
+	} catch (e) {
+		console.log(e);
+		return;
 	}
-	cakesRepository.insert(res.locals);
-
-	res.send(res.locals);
 }
 
 export default { createCake };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./App.js";
 import dotenv from "dotenv";
 dotenv.config();
 app.listen(process.env.PORT, () => {
-	console.log("Server running on port " + process.env.PORT);
+	console.log("Servidor rodando na porta " + process.env.PORT);
 });
